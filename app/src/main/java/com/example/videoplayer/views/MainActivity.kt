fe8d21ecf90b40497a2abf48f5904819dc9eb92f
Shubham Gaurav <shubham.gaurav@girnarsoft.co.in>
2022-07-26 19:28:56 +0530

    sample comment 2

--- app/src/main/java/com/example/videoplayer/views/MainActivity.kt
+++ app/src/main/java/com/example/videoplayer/views/MainActivity.kt
@@ -32,6 +32,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 //Sample comment
+//Sample comment 2
 
 class MainActivity : AppCompatActivity(), Player.Listener {
     private var exoVideoPlayer: SimpleExoPlayer? = null
